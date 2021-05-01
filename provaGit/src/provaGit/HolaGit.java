bad0746ff0030eacbffb4193c1cc6525339ab2e5
Albert Maresma <amaresma6@gmail.com>
2021-05-01 08:31:59 +0200

    Modificació del repo

--- provaGit/src/provaGit/HolaGit.java
+++ provaGit/src/provaGit/HolaGit.java
@@ -5,6 +5,10 @@ public class HolaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Git!");
+		System.out.println("Contem fins a 3");
+		for (int i = 0 ;  i < 3; i++) {
+			System.out.println(i+1);
+		}
 	}
 
 }
